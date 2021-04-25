f70579c1dc8561d654c5acfdfe062a8eca942ed9
Jan Iwanow <iwanow.jan@gmail.com>
2021-04-25 13:35:46 +0700

    implement copying to clipboard

--- URLShortener/assets/js/Form/Output.jsx
+++ URLShortener/assets/js/Form/Output.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 
 export default props => {
     if (!props.response || !props.response.url) {
         return '';
     }
     
+    const [isUrlCopiedToClipboard, setIsUrlCopiedToClipboard] = useState(false);
+    
+    const buttonClassNames = classNames({
+        'output__copy': true,
+        'output__copy--ok': isUrlCopiedToClipboard === true
+    });
+    
     return (
         <div className="output">
             <div className="output__short-url">{props.response.url}</div>
             <button
-                className="output__copy"
+                className={buttonClassNames}
                 type="button"
-                title="Copy to Clipboard">Copy to Clipboard</button>
+                title="Copy to Clipboard"
+                onClick={copyToClipboard(props.response.url, setIsUrlCopiedToClipboard)}>
+                Copy to Clipboard
+            </button>
         </div>
     );
 };
+
+function copyToClipboard(url, setIsUrlCopiedToClipboard) {
+    return async event => {
+        await navigator.clipboard.writeText(url);
+        setIsUrlCopiedToClipboard(true);
+    }
+}
